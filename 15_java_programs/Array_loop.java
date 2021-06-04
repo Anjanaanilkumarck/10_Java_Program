0df3bb8fed4d51b2c60832ba6c4a54e81d7300fa
Anjanaanilkumarck <80021787+Anjanaanilkumarck@users.noreply.github.com>
2021-06-03 23:09:13 -0400

    Add files via upload

---
+++ 15_java_programs/Array_loop.java
@@ -0,0 +1,9 @@
+//13.java program to print elements using for each loop
+
+class Array{
+public static void main(String args[])
+{
+int array[]={12,4,5,2};
+System.out.println("First element : " + array[0]);
+}
+}
